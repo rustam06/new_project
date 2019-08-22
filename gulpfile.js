31328cb4031ea8ce6d7d951f646aea7132c221a4
rustam <amirhanov06@gmail.com>
2019-08-22 20:21:35 +0300

    pulse_step_1

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,33 @@
-const gulp = require('gulp');
+const gulp        = require('gulp');
 const browserSync = require('browser-sync');
-const sass = require('gulp-sass');
-const rename = require("gulp-rename");
-const autoprefixer = require('gulp-autoprefixer');
+const sass        = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
 
-// Static server
-gulp.task('server', function () {
-    browserSync.init({
+gulp.task('server', function() {
+
+    browserSync({
         server: {
             baseDir: "src"
         }
     });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('styles', function () {
-    return gulp.src('src/sass/*.+(scss|sass)')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(rename({
-            prefix: "",
-            suffix: ".min",
-        }))
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(gulp.dest('src/css'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
-})
+});
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-    gulp.watch("src/*.html").on('change', browserSync.reload);
 })
-
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
